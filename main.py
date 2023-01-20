fa3c55cb48facd32fde9326df4dad009a9cd0b6f
Olimboy <shavkatov.olimboy@mail.ru>
2023-01-20 12:35:39 +0500

    clear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import logging
+import re
+import shutil
 import sys
 
 import config
@@ -21,6 +23,13 @@ def setup_periodic_tasks(sender, **kwargs):
         crontab(hour='*/1', minute=0),
         backup.s(),
         name='backup'
+    )
+
+    # Executes every Day.
+    sender.add_periodic_task(
+        crontab(hour='1', minute=0),
+        clear.s(),
+        name='clear'
     )
 
 
@@ -55,7 +64,40 @@ def backup():
             bot.send_document(config.RECEIVER_ID, dump_file_path_gz, caption=f'#{config.PGDATABASE}')
 
 
+@celery.task
+def clear():
+    msg = "Cleared all dumps except last 7 days"
+    error_exc = None
+
+    for date_dir in os.listdir(config.BACKUP_DIR):
+        try:
+            date = datetime.datetime.strptime(date_dir, '%d-%m-%Y')
+
+            # save last 7 days, delete if greater than
+            if date.date() < (datetime.datetime.now() - datetime.timedelta(days=7)).date():
+                shutil.rmtree(os.path.join(config.BACKUP_DIR, date_dir), ignore_errors=True)
+
+            # remove all files except last one if dir is not today's dir
+            elif date.date() != datetime.datetime.today().date():
+                for filename in os.listdir(os.path.join(config.BACKUP_DIR, date_dir))[:-1]:
+                    os.remove(os.path.join(config.BACKUP_DIR, date_dir, filename))
+
+        except Exception as e:
+            msg = '\n\n\n'.join(('Error', str(e)))
+
+    with Client("my_account",
+                config.API_ID,
+                config.API_KEY,
+                bot_token=config.BOT_TOKEN,
+                workdir=config.WORK_DIR) as bot:
+        msg = "#clear\n\n" + msg
+        bot.send_message(config.RECEIVER_ID, f'`{msg}`', parse_mode=ParseMode.MARKDOWN)
+
+
 if __name__ == '__main__':
     if '--test' in sys.argv:
         logging.basicConfig(level=logging.INFO)
-        backup()
+        if '--backup' in sys.argv:
+            backup()
+        if '--clear' in sys.argv:
+            clear()
